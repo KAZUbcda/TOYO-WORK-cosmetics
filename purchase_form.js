2e6034b8966f3ac6d6d4b60d27bac45cb6a8f293
kaede-someno <kaede.toyow@gmail.com>
2023-11-06 10:42:07 +0900

    購入機能java

---
+++ purchase_form.js
@@ -0,0 +1,26 @@
+﻿document.getElementById("button").onclick = function() {
+  let get_product = document.getElementById("get_product").textContent;
+  let get_price = document.getElementById("get_price").textContent;
+
+	localStorage.setItem("product", get_product);
+	localStorage.setItem("price", get_price);
+};
+
+// データの読み込み
+function product_load() {
+  let save_productdata = "";
+  let save_pricedata = "";
+
+  if(!localStorage.getItem("product")) {
+    save_productdata = "商品名";
+    save_pricedata = "価格";
+  } else {
+    save_productdata = localStorage.getItem("product");
+    save_pricedata = localStorage.getItem("price");
+  }
+
+  document.getElementById("set_product").innerHTML = save_productdata;
+  document.getElementById("set_price").innerHTML = save_pricedata;
+}
+
+
