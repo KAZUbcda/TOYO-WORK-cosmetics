6cbc24638bd2b427b9586a9bc58d3c26d3f2ab61
kaede-someno <kaede.toyow@gmail.com>
2023-11-13 16:53:49 +0900

    意見

--- question.js
+++ question.js
@@ -1,3 +1,12 @@
+function question_load() {
+  document.getElementById("button_back").hidden = true;
+  document.getElementById("button_send").hidden = true;
+  document.getElementById("feedback_text_yes").hidden = true;
+  document.getElementById("feedback_text_no").hidden = true;
+  document.getElementById("feedback_text_answer").hidden = true;
+  document.getElementById("question_radio").hidden = true;
+}
+
 function showContent(contentId) {
   var contents = document.querySelectorAll('.question_maincontent > div');
   for (var i = 0; i < contents.length; i++) {
@@ -9,3 +18,41 @@ function showContent(contentId) {
     content.style.display = 'block';
   }
 }
+
+document.getElementById("button_yes").onclick = function() {
+  document.getElementById("button_yes").hidden = true;
+  document.getElementById("button_no").hidden = true;
+  document.getElementById("button_back").hidden = false;
+  document.getElementById("feedback_text").hidden = true;
+  document.getElementById("feedback_text_yes").hidden = false;
+}
+
+document.getElementById("button_no").onclick = function() {
+  document.getElementById("button_yes").hidden = true;
+  document.getElementById("button_no").hidden = true;
+  document.getElementById("button_send").hidden = false;
+  document.getElementById("button_back").hidden = false;
+  document.getElementById("feedback_text").hidden = true;
+  document.getElementById("feedback_text_no").hidden = false;
+  document.getElementById("question_radio").hidden = false;
+}
+
+document.getElementById("button_send").onclick = function() {
+  document.getElementById("button_send").hidden = true;
+  document.getElementById("feedback_text_no").hidden = true;
+  document.getElementById("feedback_text_answer").hidden = false;
+  document.getElementById("question_radio").hidden = true;
+}
+
+document.getElementById("button_back").onclick = function() {
+  document.getElementById("button_yes").hidden = false;
+  document.getElementById("button_no").hidden = false;
+  document.getElementById("button_back").hidden = true;
+  document.getElementById("button_send").hidden = true;
+  document.getElementById("feedback_text").hidden = false;
+  document.getElementById("feedback_text_yes").hidden = true;
+  document.getElementById("feedback_text_no").hidden = true;
+  document.getElementById("feedback_text_answer").hidden = true;
+  document.getElementById("question_radio").hidden = true;
+}
+
